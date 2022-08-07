0e34777af418e9fd5eff42336f7c705dec3aff23
ali <alisina123kpu@gmail.com>
2022-08-07 14:33:41 +0430

    add setTimeout
    Signed-off-by: ali <alisina123kpu@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,29 @@
-import { useState ,useEffect } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlog] = useState(null)
+    const [blogs, setBlog] = useState(null);
+    const [isPending, setPending] = useState(true);
     const DeleteItem = (id) => {
         const neBlogs = blogs.filter(blog => blog.id !== id);
         setBlog(neBlogs);
     }
-    useEffect(()=>{
-        fetch(` http://localhost:8000/blogs`)
-        .then(res =>{
-            return res.json();
-        })
-        .then(data =>{
-            setBlog(data);
-        })
-    },[])
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(` http://localhost:8000/blogs`)
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBlog(data);
+                    setPending(false);
+                })
+        }, 1000);
+    }, [])
     return (
         <div className="card">
+            {isPending && <div>Loading</div>}
             <div class="card-body">
-                { blogs && <BlogList blogs={blogs} DeleteItem={DeleteItem} />}
+                {blogs && <BlogList blogs={blogs} DeleteItem={DeleteItem} />}
             </div>
         </div>
     );
