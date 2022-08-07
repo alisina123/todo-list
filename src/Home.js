fe3e4b50d893e7a5a79a77cbec01748e1ff3427e
ali <alisina123kpu@gmail.com>
2022-08-07 14:55:09 +0430

    add setError message
    Signed-off-by: ali <alisina123kpu@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlog] = useState(null);
     const [isPending, setPending] = useState(true);
+    const [error, setError] = useState(null);
     const DeleteItem = (id) => {
         const neBlogs = blogs.filter(blog => blog.id !== id);
         setBlog(neBlogs);
@@ -11,16 +12,24 @@ const Home = () => {
         setTimeout(() => {
             fetch(` http://localhost:8000/blogs`)
                 .then(res => {
+                    if (!res.ok) {
+                        throw Error("Can not fetch data");
+                    }
                     return res.json();
                 })
                 .then(data => {
                     setBlog(data);
                     setPending(false);
                 })
+                .catch(err => {
+                    setError(err.message);
+                    setPending(false);
+                })
         }, 1000);
     }, [])
     return (
         <div className="card">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading</div>}
             <div class="card-body">
                 {blogs && <BlogList blogs={blogs} DeleteItem={DeleteItem} />}
